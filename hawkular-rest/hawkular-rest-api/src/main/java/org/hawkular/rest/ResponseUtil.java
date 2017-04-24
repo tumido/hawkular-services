b1f1ebf282738e0ad7824981c6bd85988cce2e8d
Jay Shaughnessy <jshaughn@redhat.com>
2017-04-24 10:11:37 -0400

    HAWKULAR-1210 Remove legacy REST API - Simplify rest war by removing unused services and associated support - Refactor CORS support with commons-based approach

--- hawkular-rest/hawkular-rest-api/src/main/java/org/hawkular/rest/ResponseUtil.java
+++ hawkular-rest/hawkular-rest-api/src/main/java/org/hawkular/rest/ResponseUtil.java
@@ -19,28 +19,14 @@ package org.hawkular.rest;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import static javax.ws.rs.core.Response.Status.CREATED;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Spliterator;
 import java.util.stream.StreamSupport;
 
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 
-import org.hawkular.inventory.api.paging.Page;
-import org.hawkular.inventory.api.paging.PageContext;
 import org.hawkular.rest.json.ApiError;
-import org.hawkular.rest.json.Link;
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SequenceWriter;
 
 /**
  * @author Lukas Krejci
@@ -75,120 +61,6 @@ final class ResponseUtil {
         return Response.status(CREATED)
                 .entity(StreamSupport.stream(ids, false).map(
                         (id) -> info.getRequestUriBuilder().segment(id).build()));
-    }
-
-    public static <T> Response.ResponseBuilder pagedResponse(Response.ResponseBuilder response, UriInfo uriInfo,
-                                                             ObjectMapper mapper, Page<T> page) {
-        InputStream data = null;
-        try {
-            //extract the data out of the page
-            data = pageToStream(page, mapper, () -> {
-                // the page iterator should be depleted by this time so the total size should be correctly set
-                response.type(MediaType.APPLICATION_OCTET_STREAM_TYPE);
-                createPagingHeader(response, uriInfo, page);
-            });
-        } catch (IOException e) {
-        }
-        response.entity(data);
-        return response;
-    }
-
-    public static <T> Response.ResponseBuilder pagedResponse(Response.ResponseBuilder response, UriInfo uriInfo,
-                                                             Page<T> page, Object data) {
-        response.entity(data);
-        createPagingHeader(response, uriInfo, page);
-        return response;
-    }
-
-    private static <T> InputStream pageToStream(Page<T> page, ObjectMapper mapper, Runnable callback) throws
-            IOException {
-        final PipedOutputStream outs = new PipedOutputStream();
-        final PipedInputStream ins = new PipedInputStream() {
-            @Override public void close() throws IOException {
-                outs.close();
-                super.close();
-            }
-        };
-        outs.connect(ins);
-        mapper.disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
-
-        PageToStreamThreadPool.getInstance().submit(() -> {
-            try (Page<T> closeablePage = page;
-                 PipedOutputStream out = outs;
-                 SequenceWriter sequenceWriter = mapper.writer().writeValuesAsArray(out)) {
-                for (T element : closeablePage) {
-                    sequenceWriter.write(element);
-                    sequenceWriter.flush();
-                }
-            } catch (IOException e) {
-                throw new IllegalStateException("Unable to convert page to input stream.", e);
-            } finally {
-                callback.run();
-            }
-        });
-        return ins;
-    }
-
-    /**
-     * Create the paging headers for collections and attach them to the passed builder. Those are represented as
-     * <i>Link:</i> http headers that carry the URL for the pages and the respective relation.
-     * <br/>In addition a <i>X-Total-Count</i> header is created that contains the whole collection size.
-     *
-     * @param builder    The ResponseBuilder that receives the headers
-     * @param uriInfo    The uriInfo of the incoming request to build the urls
-     * @param resultList The collection with its paging information
-     */
-    public static void createPagingHeader(final Response.ResponseBuilder builder, final UriInfo uriInfo,
-                                          final Page<?> resultList) {
-
-        UriBuilder uriBuilder;
-
-        PageContext pc = resultList.getPageContext();
-        int page = pc.getPageNumber();
-
-        List<Link> links = new ArrayList<>();
-
-        if (pc.isLimited() && resultList.getTotalSize() > (pc.getPageNumber() + 1) * pc.getPageSize()) {
-            int nextPage = page + 1;
-            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
-            uriBuilder.replaceQueryParam("page", nextPage);
-
-            links.add(new Link("next", uriBuilder.build().toString()));
-        }
-
-        if (page > 0) {
-            int prevPage = page - 1;
-            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
-            uriBuilder.replaceQueryParam("page", prevPage);
-            links.add(new Link("prev", uriBuilder.build().toString()));
-        }
-
-        // A link to the last page
-        if (pc.isLimited()) {
-            long lastPage = resultList.getTotalSize() / pc.getPageSize();
-            if (resultList.getTotalSize() % pc.getPageSize() == 0) {
-                lastPage -= 1;
-            }
-
-            uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
-            uriBuilder.replaceQueryParam("page", lastPage);
-            links.add(new Link("last", uriBuilder.build().toString()));
-        }
-
-        // A link to the current page
-        uriBuilder = uriInfo.getRequestUriBuilder(); // adds ?q, ?per_page, ?page, etc. if needed
-
-        StringBuilder linkHeader = new StringBuilder(new Link("current", uriBuilder.build().toString())
-                .rfc5988String());
-
-        //followed by the rest of the link defined above
-        links.forEach((l) -> linkHeader.append(", ").append(l.rfc5988String()));
-
-        //add that all as a single Link header to the response
-        builder.header("Link", linkHeader.toString());
-
-        // Create a total size header
-        builder.header("X-Total-Count", resultList.getTotalSize());
     }
 
     public static Response internalError(String message) {
